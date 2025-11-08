9fe5f6aea46e12e0ef294d2f7761e6fb5536bd84
riparazionidigitalisrls-debug <riparazionidigitali.srls@gmail.com>
2025-11-09 00:49:34 +0100

    Update scraper_componenti_enterprise.js

--- scraper_componenti_enterprise.js
+++ scraper_componenti_enterprise.js
@@ -574,7 +574,7 @@ class ScraperComponentiEnterprise {
     });
 
     try {
-      const context = await browser.newContext({
+      const context = await this.browser.newContext({
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         viewport: { width: 1920, height: 1080 }
       });
